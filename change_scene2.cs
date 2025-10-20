d1566c570871bb8e5dcc5384f86fe66d2b4c75da
sebassordo123 <jsordonez.2265@unicesmag.edu.co>
2025-10-20 10:58:51 -0500

    Add files via upload

---
+++ change_scene2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class change_scene2 : MonoBehaviour
+{
+
+    public void changemenuscene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
